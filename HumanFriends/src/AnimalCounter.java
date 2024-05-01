4ec326268326274a88c0392f55d0550d04525e76
Michael_Fesenko <@gmail.com>
2024-05-01 18:01:44 +0300

    Revert "some fixes HumanFriends"
    This reverts commit 7ed7bd6e9648c9187b4cc3c9b3236452562b93c3.

--- HumanFriends/src/AnimalCounter.java
+++ HumanFriends/src/AnimalCounter.java
@@ -2,18 +2,24 @@ public class AnimalCounter implements AutoCloseable {
     private int count = 0;
     private boolean isClosed = false;
 
-    public void add() {
+    public void incrementCount(Animal animal) {
         if (isClosed) {
-            throw new IllegalStateException("Невозможно увеличить счетчик.");
+            throw new IllegalStateException("Счетчик животных закрыт. Невозможно увеличить счетчик.");
         }
-        count++;
+        if (isValidAnimal(animal)) {
+            count++;
+        }
     }
 
     public int getCount() {
         if (isClosed) {
-            throw new IllegalStateException("Невозможно получить значение счетчика.");
+            throw new IllegalStateException("Счетчик животных закрыт. Невозможно получить значение счетчика.");
         }
         return count;
+    }
+
+    private boolean isValidAnimal(Animal animal) {
+        return animal.name != null && !animal.name.isEmpty() && animal.birthDate != null;
     }
 
     @Override
